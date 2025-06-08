c3dfa7547f066f1051a16b2725a54f028c6df272
Nurlan <nurlannapo@gmail.com>
2025-06-09 02:06:22 +0500

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,8 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://shslwdxurecahtcejxpy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNoc2x3ZHh1cmVjYWh0Y2VqeHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1NTUwMzAsImV4cCI6MjA2MDEzMTAzMH0.dUl1D_OQ3UZZGvcyo915c9YTxz6r28npoJFuD1r2Ztc';
+const supabaseUrl = 'SUPABASEURL';
+const supabaseAnonKey = 'ANONKEY';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
